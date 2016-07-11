c08ee0599321435d77da6f6191e66a8015628166
Anthony <moz4r@free.fr>
2016-07-11 19:01:20 +0200

    Rachel bot stable POC version (#5)
    * test
    
    ajout question âge
    
    * ajout reponses age
    
    * add php fetchers sources
    
    * ajout aiml nombre
    
    il sait calculer ce qu'on lui demande
    
    * ajout remerciement et petite retouche
    
    remerciement , répond à merci
    
    météo --->  #météo# , pour que même intégré dans une phrase il comprenne
    
    * paupieres
    
    * webkitspeech stop listening when speak
    
    * ajout aiml phrases et expression aléatoires
    
    * commit sync
    
    * random life - WIP
    
    * Vie_aléatoire-Random_life ( TEST )
    
    * ajouts
    
    ---correction prononciation dans personalite basique
    ---ajout d'un aiml ou l'on peux y inclure des blagues
    ---ajout de possibilité d'apprentissage et du mot "et" qui est souvent
    pris à la place de "est"
    
    * English-version ( just the minimum )
    
    * [clean up] Bascule des phrases systeme dans l'aiml
    
    exemple "je ne comprend pas" ...
    + desactivation temporaire de la recherche internet
    
    * Refonte des blagues [french jokes]
    
    pour rendre compatible le gros fichier de blagues
    
    * [weather bug] météo ajout de la ville dans la recherche
    
    il reste encore un petit soucis à gérér la météo semble la même quelque
    soit la ville demandée...
    
    * commande d extinction de"vie aleatoire"
    
    FERME TA GUEULE pour eteindre et DEBOUT pour reprendre
    
    * modif micro [webkitspeech]
    
    * Petit nettoyage [ clean up ]
    
    * Petites modifications
    
    random life
    
    * Ajout de fonctionalité DATE et HEURE
    
    Quelle heure #
    Quel jour sommes #
    
    * Sound ADDED
    
    demande "QUI EST MON PÈRE"
    
    * R2D2 inside
    
    sound mod
    
    * mise a jour :)
    
    * lui dire " dis moi un truc "
    
    mélange aléatoire de mots qui forment des phrases ( test , ce n'est pas
    forcement un fichier a garder )
    
    * petits bugs corigés
    
    * correction
    
    * du chipotage XD
    
    * correction de blagues + comment vas tu
    
    * correction pour la prononciation "apéro" XD
    
    * Ne fonctionne pas encore [learning wip]
    
    * Aprentissage / Learning [good for test]
    
    * Aprentissage / learning - test2
    
    * Un petit oubli :)
    
    situation
    
    * [AIMLIF BUG FIX]
    
    on est plus obligés de supprimmer les fichiers aimlif a chaque fois
    ATTENTION pensser a bien enregistrer en UT8 les fichiers AIML a chaque
    fois . car le programme les converti a chaque demarrage
    
    * AIML BUG FIX voir comit précédent
    
    * modif utf8
    
    * betises réparée [ut8 aiml convert]
    
    * recup
    
    * apprentisage partagé préparation
    
    * [Learning WIP ProgramAB server added ] - Aprentissage MAJ 2/3
    
    Ca prend forme
    
    * Aprentissage 3/4
    
    RESTE DES BUG  avec les accents
    
    * [VERSION STABLE] pour test  - apprentissage 4/4
    
    * small startup fix
    
    * Inmoov messenger 1/2
    
    Préparation du module
    
    * [inmoov messenger] 2/2
    
    on ne peut pas encore envoyer de messages mais on peut en recevoir :)
    
    * [Update version check][Prévenir l'utilisateur d'une mise à jour]
    
    * [webkit micro] Fix
    
    Version 1.5
    plus besoin de cliquer sur le micro
    
    * [wip] Inmoov messenger
    
    * mise a jour de l'apprentissage
    
    avec entre autre "c est quoi * "
    
    * correction & ajout de phrases
    
    * chipotage
    
    * inmoov messenger
    
    + corrections
    
    * traduction en cours version anglaise
    
    * fix
    
    problem messenger quand on utilise plusieurs platorme + traduction
    
    * English translation + fixes

---
+++ bots/INTERNET-FETCHERS/base.php
@@ -0,0 +1,129 @@
+<?php
+header("Content-Type: text/html;charset=UTF-8");
+header("Cache-Control: max-age=0");
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+include 'phpdom_dependencies.php';
+$sortie=0;
+$pic="none_xyz";
+$lang="FR";
+$CITY="PARIS";
+$units="metric";
+$genre="RANDOM";
+$row_cnt=0;
+$type="";
+
+$bdd = mysqli_connect('localhost', 'myai', 'password', 'myai');
+mysqli_set_charset($bdd,"utf8");
+
+if (isset($_GET["type"]))
+	{
+	if ($_GET["type"]<>"")
+		{
+		$type=$_GET["type"];
+		}
+	}
+if (isset($_GET["genre"]))
+	{
+	if ($_GET["genre"]<>"")
+		{
+		$genre=$_GET["genre"];
+		}
+	}
+if (isset($_GET["pic"]))
+	{
+	if ($_GET["pic"]<>"")
+		{
+		$pic=$_GET["pic"];
+		}
+	}
+if (isset($_GET["lang"]))
+	{
+	if ($_GET["lang"]<>"")
+		{
+		$lang= strtoupper($_GET["lang"]);
+		}
+	}
+if (isset($_GET["CITY"]))
+	{
+	if ($_GET["CITY"]<>"")
+		{
+		$CITY=$_GET["CITY"];
+		}
+	}	
+if (isset($_GET["units"]))
+	{
+	if ($_GET["units"]<>"")
+		{
+		$units=$_GET["units"];
+		}
+	}	
+if (isset($_GET["question"]))
+	{
+	if ($_GET["question"]<>"")
+		{
+		$question=$_GET["question"];
+		}
+	}
+	
+
+	
+	
+switch ($type){
+	
+include 'pics.php';
+include 'weather.php';
+include 'y_awsers.php';	
+
+	
+}
+	
+	
+	
+function remplaceGuillemets($texte)
+{
+    //Places chaque caractères du texte dans un tableau
+    $listeCaracteres = str_split($texte, 1);
+ 
+    //Créé un tableau contenant la position de chaque guillemets
+    $rechercheGuillemets = array_keys($listeCaracteres, '"');
+ 
+    //Parcours le tableau contenant la position des guillemets
+    foreach ($rechercheGuillemets as $key => $value)
+    {
+        //Remplace alternativement par des guillemets ouvrante ou fermante
+        if($key%2 == 0){ $listeCaracteres[$value] = '« '; }
+        else{ $listeCaracteres[$value] = ' »'; }
+    }
+ 
+    //Reconstitue le texte
+    return implode('', $listeCaracteres);
+}
+
+function check_urlimage($urlimage,$picV,$langV,$conn)
+{
+$UrlTmp=$urlimage;	
+if (@getimagesize($urlimage))
+{
+	
+}
+else
+{
+$UrlTmp=file_get_contents('http://bot1.myai.cloud:9090/?pic='.urlencode ($picV));
+}
+updatecache($picV,$UrlTmp,$langV,$conn);	
+return $UrlTmp;
+}
+
+function updatecache($nomV,$urlV,$langV,$conn)
+{
+if ($urlV<>""&&$urlV<>"0")
+{
+mysqli_query($conn,"INSERT INTO pic_fr (nom, url, lang) VALUES('".$nomV."','".$urlV."','".$langV."') ON DUPLICATE KEY UPDATE nom='".$nomV."', url='".$urlV."' , lang='".$langV."'");	
+}
+}
+
+
+
+
+echo $sortie;
