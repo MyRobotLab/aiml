d461d02ed8d9e9ed6ccce73eb328f4b47397eef4
Anthony <moz4r@free.fr>
2016-08-09 02:53:46 +0200

    [lecagnois] refonte loto

--- bots/BOTS-FRENCH/Inmoov_AI/PYTHON/1-INMOOV-AI_startup.py
+++ bots/BOTS-FRENCH/Inmoov_AI/PYTHON/1-INMOOV-AI_startup.py
@@ -5,7 +5,7 @@
 # STABLE FILES : https://github.com/MyRobotLab/pyrobotlab/tree/master/home/moz4r  [ RACHEL AIML + PYTHON ]
 # UPDATED DEV FILES :  https://github.com/MyRobotLab/aiml/tree/master/bots/ [ RACHEL AIML + PYTHON ]
 # -----------------------------------
-# - Inmoov-AI Version 1.7 By Moz4r
+# - Inmoov-AI Version 1.7.2 By Moz4r
 # - Credit :
 # - Rachel the humanoïde
 # - Wikidatafetcher By Beetlejuice
@@ -14,6 +14,7 @@
 # - HairyGael
 # - Heisenberg
 # - Grattounet
+# - Lecagnois
 # -----------------------------------
 # !!! INSTALL : !!!
 # !!! PLEASE copy all aiml files to : develop\ProgramAB\bots\rachel\aiml !!!
@@ -669,14 +670,45 @@ def question(data):
 
 
 
-def loto(phrase,the,lucky):
+def loto(phrase,the,chance,fin):
+	table1 = [(random.randint(1,49)), (random.randint(1,49)), (random.randint(1,49)), (random.randint(1,49)),(random.randint(1,49))]
+	tablefin = []
+	doublon = []
+
+	for i in table1:
+		if i not in tablefin:
+			tablefin.append(i) #supprime les doublons
+		else:
+			doublon.append(i) #extraire les doublons
+			d = len(doublon)
+			while d > 0:
+			#nouveau tirage
+				doublon = []
+				table1 = [(random.randint(1,49)), (random.randint(1,49)), (random.randint(1,49)), (random.randint(1,49)),(random.randint(1,49))]
+				# recherche doublon
+				for i in table1:
+					if i not in tablefin:
+						tablefin.append(i) #supprime les doublons
+					else:
+						doublon.append(i) #extraire les doublons
+					# si il existe doublon d+1 et vite la table
+					if (len(doublon)==1)or(len(doublon)==2)or(len(doublon)==3)or(len(doublon)==4)or(len(doublon)==5):
+						talkBlocking("j ai trouver un doublon , je refais un tirage")
+						d = d+1
+						doublon =[]
+					else:
+						d = 0
+		break
+	# tri la table avant de la dire
+	table1.sort()
 	talkBlocking(phrase)
-	talkBlocking(the+str(random.randint(1,49)))
-	talkBlocking(the+str(random.randint(1,49)))
-	talkBlocking(the+str(random.randint(1,49)))
-	talkBlocking(the+str(random.randint(1,49)))
-	talkBlocking(the+str(random.randint(1,49)))
-	talkBlocking(lucky+str(random.randint(1,10)))
+	talkBlocking(the+str(table1[0]))
+	talkBlocking(the+str(table1[1]))
+	talkBlocking(the+str(table1[2]))
+	talkBlocking(the+str(table1[3]))
+	talkBlocking(the+str(table1[4]))
+	talkBlocking(chance+str(random.randint(1,9)))
+	talkBlocking(fin)
 
 def DisplayPic(pic):
 	r=0
