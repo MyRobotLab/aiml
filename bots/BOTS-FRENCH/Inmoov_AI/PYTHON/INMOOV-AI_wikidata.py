8434d121d6e1b2f62db48d505d68d2ad751779f4
moz4r ( Anthony ) <moz4r@free.fr>
2016-09-08 17:17:50 +0200

    [WIKIDATA] affinage et correction de bugs
    Désomais cela fonctionne :
    - C est quoi des chevaux
    - Qui est alan turing
    - Qui est le président de la france
    - Qui est le premier ministre du mexique

--- bots/BOTS-FRENCH/Inmoov_AI/PYTHON/INMOOV-AI_wikidata.py
+++ bots/BOTS-FRENCH/Inmoov_AI/PYTHON/INMOOV-AI_wikidata.py
@@ -7,20 +7,37 @@ def askWiki(query,question,ReturnOk,ReturnNok): # retourne la description du suj
 		query2 = query[2:len(query)]
 		query = query2
 	print query # petit affichage de contrôle dans la console python ..
-	word = wdf.cutStart(query) # on enleve le derminant ("le chat" -> "chat")
-	wordSingular = word=Singularize(word) # on met au singulier pour double test
+	
+	
 	start = wdf.grabStart(query) # on garde que le determinant ( je ne sais plus pourquoi j'ai eu besoin de ca, mais la fonction existe ...)
 	# coucou fred je vais m en servir en tous cas :) bon a terme faudra un autre dico pour detecter le feminin/masculin du premier mot du retour wiki
-	retour = " est un "
+	
+	WeCutTheFirstWord=0 #on enleve le/la/les etc... uniqument si présent
+	retour = " est un, ou une, "
 	if start=="du":
 		start="le"
-		retour=" est un "
+		retour=" est un, ou une, "
+		WeCutTheFirstWord=1
+	if start=="le":
+		retour=" est un, ou une, "
+		WeCutTheFirstWord=1
+	if start=="la":
+		retour=" est un, ou une, "
+		WeCutTheFirstWord=1
 	if start=="des":
 		start="les"
 		retour=" sont des "
+		WeCutTheFirstWord=1
 	if start=="les":
 		retour=" sont des "
+		WeCutTheFirstWord=1
 
+	if WeCutTheFirstWord==1:
+		word = wdf.cutStart(query) # on enleve le derminant ("le chat" -> "chat")	
+	else:
+		word=query
+	wordSingular = word=Singularize(word) # on met au singulier pour double test
+	
 	wikiAnswer = wdf.getDescription(word) # recupere la description su wikidata
 	answer = ( query + retour + wikiAnswer)
 	print wikiAnswer,answer
@@ -38,26 +55,29 @@ def getProperty(query, what): # retourne la valeur contenue dans la propriete de
 	#Light(1,0,0)
 	query = unicode(query,'utf-8')
 	what = unicode(what,'utf-8')
+	
 	if query[1]== "\'" :
 		query2 = query[2:len(query)]
 		query = query2
 	if what[1]== "\'" :
 		what2 = what[2:len(what)]
 		what = what2
-		print "what = " + what + " - what2 = " + what2
+	
 	ID = "error"
-	# le fichier propriete.txt contient les conversions propriete -> ID . wikidata n'utilise pas des mots mais des codes (monnaie -> P38)	f = codecs.open(unicode('os.getcwd().replace("develop", "").replace("\", "/") + "/proprietes_ID.txt','r',"utf-8") #
+	# le fichier WIKIprop.txt contient les conversions propriete -> ID . wikidata n'utilise pas des mots mais des codes (monnaie -> P38)	f = codecs.open(unicode('os.getcwd().replace("develop", "").replace("\", "/") + "/proprietes_ID.txt','r',"utf-8") #
 	f = codecs.open(oridir+WikiFile,'r','utf-8') #os.getcwd().replace("develop", "").replace("\\", "/") set you propertiesID.txt path
 	
 	for line in f:
     		line_textes=line.split(":")
     		if line_textes[0]== what:
 	    		ID= line_textes[1]
+				
 	f.close()
 	#print "query = " + query + " - what = " + what + " - ID = " + ID
 	wikiAnswer= wdf.getData(query,ID) # recupere la valeur de la propriete si elle existe dans le document
-	answer = ( what +" de " + query + " est " + wikiAnswer)
 	
+	answer = ( what +" de " + query + " est " + wikiAnswer)
+	print ID,answer,what,query,wikiAnswer
 	if wikiAnswer == "Not Found !":
 		answer=(question(query+" "+what))
 		sleep(1);
