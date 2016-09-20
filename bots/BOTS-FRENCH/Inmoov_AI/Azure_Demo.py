5834f86653302908abea26250289361a839bdf1b
Anthony <moz4r@free.fr>
2016-09-20 13:04:52 +0200

    Update Azure_Demo.py

--- bots/BOTS-FRENCH/Inmoov_AI/Azure_Demo.py
+++ bots/BOTS-FRENCH/Inmoov_AI/Azure_Demo.py
@@ -7,7 +7,7 @@ Runtime.createAndStart("mouth", "AcapelaSpeech")
 AzureTranslator=Runtime.createAndStart("AzureTranslator", "AzureTranslator")
 sleep(0.1)
 #initiate azure
-AzureTranslator.setCredentials("7da9defb-7d86-46e4-8607-5b82f280f430","IgCffuxo0QgHDzKMK1k5bf9/9GpP9ZwWwAaonacAcW0") # KEY and SECRET azure credentials
+AzureTranslator.setCredentials("YOUR_KEY_HERE_7da9defb-7d86-etc...","YOUR_SECRET_HERE_IgCffuxo-etc...") # KEY and SECRET azure credentials
 #voice output
 mouth.setVoice("Ryan")
 mouth.setLanguage("EN")
@@ -76,7 +76,7 @@ def translateText(text,language):
 	try:
 		RealLang=en_languages[language]
 	except: 
-		mouth.speak("There is a problem with azure, i am so sorry")
+		mouth.speak("I dont know this language, i am so sorry, or you made a mistake dude")
 	print RealLang
 	if RealLang!="0":
 		AzureTranslator.toLanguage(RealLang)
@@ -94,7 +94,7 @@ def translateText(text,language):
 
 
 		if 'Cannot find an active Azure Market Place' in t_text:
-			mouth.speak("There is a problem with azure, i am so sorry")
+			mouth.speak("There is a problem with azure, i am so sorry. Or maybe I am tired")
 		else:
 			#change acapela voice
 			mouth.setVoice(male_languages[RealLang])  
